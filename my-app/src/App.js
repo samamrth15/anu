f0163d12f63e14bf378566afee4c68bc3223fc66
sa <aa>
2023-12-11 16:50:01 +0530

    arya

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+
+const Greeting = () => {
+  return <h1>Hello, Welcome to My React App!</h1>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Greeting />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
